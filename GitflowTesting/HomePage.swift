73b2625dc3547dfc5219935b25c5c8f03a1fd430
ouhkstudent <s1174159@ouhk.edu.hk>
2019-02-16 00:05:07 +0800

    Initial Commit

---
+++ GitflowTesting/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  GitflowTesting
+//
+//  Created by yu fai on 14/2/2019.
+//  Copyright © 2019 YuFai. All rights reserved.
+//
+
+import UIKit
+
+class HomePage: UIViewController {
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = UIColor.red
+        
+    }
+    
+}
